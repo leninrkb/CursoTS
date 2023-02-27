8f803b823eb0e29073f34fc2233adf80d68f6ade
Lenin Acosta <rkb.lenin@gmail.com>
2023-02-27 17:57:30 -0500

    string literal

--- string_literal.ts
+++ string_literal.ts
@@ -1,31 +1,73 @@
-let myvar:'lenin';
-myvar='lenin'
+let myFavoritePet:'cat';
+myFavoritePet='cat'
 
 type Specie = 'cat' | 'dog' | 'bird';
 interface Pet{
-  species:Species;
+  specie:Specie;
+  name:string;
   eat();
   sleep();
+  walk();
 }
 interface Cat extends Pet{
-  species:'cat'
+  specie:'cat'
   
 }
 interface Bird extends Pet{
-  species:'bird'
+  specie:'bird'
   sing()
 }
+
 function petIsCat(pet:Pet): pet is Cat{
-  return pet.species === 'cat'
+  return pet.specie === 'cat'
+}
+function petIsBird(pet:Pet): pet is Bird{
+  return pet.specie === 'bird'
 }
 function buyPet(pet:Specie, name:string):Pet{
-	return name;
+  if (pet === 'cat'){
+    return {
+      specie:'cat',
+      name:this.name,
+      eat: function (){
+        console.log(`meow meow ${this.name} drinks milk`);
+        
+      },
+      sleep:function(){
+        console.log(`meow meow ${this.name} now sleeps`);
+      }
+    } as Cat;
+  }else if (pet === 'bird'){
+    return {
+      specie:'bird',
+      name:this.name,
+      eat:function(){
+        console.log(`${this.name} eats a cookie`);
+      },
+      sing:function(){
+        console.log(`${this.name} is happy and sings`);
+      }
+    } as Bird
+  }else{
+    throw `lo sentimos, no tenemos un ${pet} o no se que monda`;
+    
+  }
 }
 buyPet('cat','bibi');
+buyPet('dog','bibi');
 
-function buyPet(pet: Species, name: string) : Pet;
-function buyPet(pet: "cat", name: string): Cat;
-function buyPet(pet: "dog", name: string): Dog;
-function buyPet(pet: "bird", name: string): Bird;
-function buyPet(pet: Species, name: string) : Pet { /*...*/ }
-let dog = buyPet(myFavoritePet /* "dog" as defined above */, "Rocky");
+
+let cat = buyPet(myFavoritePet, "Rocky");
+
+
+function playWithPet(pet:Pet){
+  console.log(`hey ${pet.name}, vamoa juga`);
+  
+  if(petIsCat(pet)){
+    pet.eat()
+    pet.sleep()
+  }else if(petIsBird(pet)){
+    pet.eat()
+    pet.sing()
+  }
+}
